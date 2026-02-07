2644c002dc22ae9c3a237c5c66ac5dc177613c2c
Jose Mac <koatitechcrcostarica@gmail.com>
2026-02-07 12:21:54 -0600

    fix: Enhance text wrapping and display for remate date and title elements in CSS.

--- Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd694ee8c6603d68d5d0340aeb78610181ea4745")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fb01a2d4c4abb3b7711e4a3fa497e38b5f0f8cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
