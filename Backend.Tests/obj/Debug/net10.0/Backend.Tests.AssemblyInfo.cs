b57f80935091c50ae9224570ed6a7cf13abee7a2
Jose Mac <koatitechcrcostarica@gmail.com>
2026-02-07 11:20:15 -0600

    ok

--- Backend.Tests/obj/Debug/net10.0/Backend.Tests.AssemblyInfo.cs
+++ Backend.Tests/obj/Debug/net10.0/Backend.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b1685106c72cfb2966212c32493f5fe48ceb748")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3556296f75ddd8612cf59b4adc1678b65991105c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
