9c34ae660115c87893807e339e1a44aed8ed1c46
Jose Mac <koatitechcrcostarica@gmail.com>
2026-02-08 13:52:26 -0600

    Update build artifacts and cache files for the Backend project.

--- Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c02a5ddf98a0aa99cbb26cc8146c0b5a39079e60")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4025d7b7f4c0765f281d90ec30e49354a76da41e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
