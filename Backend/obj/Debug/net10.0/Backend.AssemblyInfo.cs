eb018c84d8ac404d1cc160ee6d6a385d48464308
Jose Mac <koatitechcrcostarica@gmail.com>
2026-02-08 13:33:20 -0600

    chore: Rebuild Backend project and update VS Code launch and task configurations.

--- Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+344b48065b759cafe1dbae9110752c91b58b8678")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45d72fe47efd00e480f0362eed003a93b2883845")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
