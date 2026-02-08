4025d7b7f4c0765f281d90ec30e49354a76da41e
Jose Mac <koatitechcrcostarica@gmail.com>
2026-02-08 13:46:25 -0600

    config: Specify application URL as http://localhost:5033 in appsettings.json.

--- Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb018c84d8ac404d1cc160ee6d6a385d48464308")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c02a5ddf98a0aa99cbb26cc8146c0b5a39079e60")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
