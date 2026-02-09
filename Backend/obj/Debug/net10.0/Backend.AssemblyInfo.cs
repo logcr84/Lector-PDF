29425f119d2262b144a67f363f299466a486acdc
Jose Mac <koatitechcrcostarica@gmail.com>
2026-02-08 20:03:12 -0600

    feat: Add Swashbuckle for API documentation and PdfPig for PDF processing.

--- Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40b5b188b0636c92d9cf19ed1a35c4a67fe4bd56")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e838733d3440ef5c524560b30934f4935e749c51")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
