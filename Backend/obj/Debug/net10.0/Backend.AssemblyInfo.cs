3d4cfa0d4462d6c4d5bbeb2495bee713334d6d3a
Jose Mac <koatitechcrcostarica@gmail.com>
2026-02-13 14:51:40 -0600

    Update generated assembly informational versions and build artifacts.

--- Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+349cf46fde8692991bdc56cf8c816a026b669406")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+167c9ddd2bc532690feaa73a393b636e76dd285f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
