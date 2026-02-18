21c51f6ceb684529c6d783610a260fe26055dff5
Jose Mac <koatitechcrcostarica@gmail.com>
2026-02-18 15:19:24 -0600

    build: update assembly informational version and build cache.

--- Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8820581fd276f1e9be09326fafcd30f79084a6e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dedb8c41391fda8db36522b930870712eff2d061")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
